06460cf2ecca8ced01a25a1fed29188abff98280
filippogambarota <filippo.gambarota@gmail.com>
2023-08-06 13:10:49 +0200

    adding fil

---
+++ R/info.R
@@ -0,0 +1,20 @@
+#' fil
+#' @description
+#' Store my public info useful within projects
+#'
+#' @return a named list with all info
+#' @export
+#'
+fil <- function(){
+  list(
+    bib = "https://raw.githubusercontent.com/filippogambarota/bib-database/main/references.bib",
+    csl = "https://raw.githubusercontent.com/citation-style-language/styles/master/apa.csl",
+    orcid = "0000-0002-6666-1747",
+    twitter = "fgambarota",
+    github = "filippogambarota"
+  )
+}
+
+
+
+

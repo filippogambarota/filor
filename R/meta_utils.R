f7c319736dc6744384473ad8a17c2978616790d1
Filippo Gambarota <filippo.gambarota@gmail.com>
2023-08-30 12:05:32 +0200

    improving tau2_from_I2 docs

--- R/meta_utils.R
+++ R/meta_utils.R
@@ -9,12 +9,12 @@
 #' @examples
 #' tau2_from_I2(0.2, 0.08)
 tau2_from_I2 <- function(I2, vi){
-  (I2 * vi) / (1 - I2)
+  -((I2 * vi) / (I2 - 1))
 }
 
 #' I2
 #'
-#' @param tau the standard error (tau)
+#' @param tau2 the heterogeneity
 #' @param vi the typical sampling variability
 #'
 #' @return numeric the I^2 value
@@ -22,8 +22,8 @@ tau2_from_I2 <- function(I2, vi){
 #'
 #' @examples
 #' I2(0.1, 0.08)
-I2 <- function(tau, vi){
-  tau^2 / (tau^2 + vi)
+I2 <- function(tau2, vi){
+  tau2 / (tau2 + vi)
 }
 
 #' sim_meta
@@ -97,4 +97,70 @@ compare_rma <- function(...){
   setNames(out, fitnames)
 }
 
+#' summary_rma
+#'
+#' @param x a model fitted with \code{metafor::rma.uni()}
+#' @param extra_params a vector of (extra) parameters to be extracted (see details). These are the elements of the \code{rma.uni} objects.
+#' @details
+#' By default the function extract \code{c("b", "se", "zval", "pval", "ci.lb", "ci.ub")} as parameters.
+#' @return a dataframe
+#' @export
+#'
+summary_rma <- function(x,
+                         extra_params = NULL) {
 
+  if(x$model != "rma.uni"){
+    stop("The function currently support only rma.uni models")
+  }
+
+  params <- c("b", "se", "zval", "pval", "ci.lb", "ci.ub")
+
+  if(!is.null(extra_params)){
+    extra_params <- base::setdiff(extra_params, params)
+    params <- c(params, extra_params)
+  }
+
+  fixefs <- lapply(params, function(p) if(is.numeric(x[p])) as.numeric(x[p]) else x[p])
+  names(fixefs) <- params
+  fixefs <- data.frame(fixefs)
+
+  if (is.null(x$model)) {
+    sigmas <- x$sigma2
+  }
+  else {
+    sigmas <- x$tau2
+  }
+
+  if(x$model == "rma.uni"){
+    fixefs$I2 <- x$I2
+  }
+
+  if(length(sigmas) > 1){
+    names(sigmas) <- paste0("tau2_", 1:length(sigmas))
+  }else{
+    names(sigmas) <- "tau2"
+  }
+
+  out <- cbind(fixefs, t(data.frame(sigmas)))
+  rownames(out) <- NULL
+  return(out)
+}
+
+#' compare_rma
+#' @description
+#' Put in a dataframe one or more models fitted with \code{metafor::rma.uni} for a nice visual comparison.
+#'
+#' @param ... models to be compared
+#' @param extra_params a vector of (extra) parameters to be extracted. These are the elements of the \code{rma.uni} objects.
+#'
+#' @return a dataframe
+#' @export
+#'
+compare_rma <- function(..., extra_params = NULL) {
+  fitnames <- as.list(substitute(...()))
+  fits <- list(...)
+  sums <- lapply(fits, summary_rma, extra_params = extra_params)
+  sums <- do.call(rbind, sums)
+  out <- data.frame(t(sums))
+  setNames(out, fitnames)
+}
